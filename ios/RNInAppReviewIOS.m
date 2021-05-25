e19f8c92043e84395608b2bde1e787c1035efee0
Bruce Oh <brucegwanhooh@acloset.app>
2021-05-25 14:20:39 +0900

    update library

--- ios/RNInAppReviewIOS.m
+++ ios/RNInAppReviewIOS.m
@@ -21,13 +21,42 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(requestReview:
     resolver:(RCTPromiseResolveBlock)resolve
     rejecter:(RCTPromiseRejectBlock)reject) {
-      if (@available(iOS 10.3, *)) {
-        [SKStoreReviewController requestReview];
-         resolve(@"true");
-      }else{
-         reject(@"21",@"ERROR_DEVICE_VERSION",nil);
-      }
+      
+  
+  
+  if (@available(iOS 14, *)) {
+         [self logMessage:@"iOS 14+"];
+         UIWindowScene *scene = [self findActiveScene];
+         if (scene) {
+             [self logMessage:@"scene found"];
+             [SKStoreReviewController requestReviewInScene:scene];
+             result(nil);
+         } else {
+             [self logMessage:@"scene not found"];
+           result(nil);
+         }
+     } else if (@available(iOS 10.3, *)) {
+         [self logMessage:@"iOS 10.3+"];
+         [SKStoreReviewController requestReview];
+         result(nil);
+     } else {
+       result(nil);
+     }
 }
+
+- (UIWindowScene *) findActiveScene  API_AVAILABLE(ios(13.0)){
+    for (UIWindowScene *scene in UIApplication.sharedApplication.connectedScenes) {
+        
+        if (scene.activationState == UISceneActivationStateForegroundActive) {
+            return scene;
+        }
+        
+    }
+    
+    return nil;
+}
+
+
 
 + (BOOL)requiresMainQueueSetup
 {
